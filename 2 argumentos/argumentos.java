e4271d7905ad0c66a469bcc1ef70dc2a8016c44d
Cuelholima <matth.faria96@gmail.com>
2018-03-14 11:37:12 -0300

    adding my first and second program in java

---
+++ 2 argumentos/argumentos.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Matheus Henrique de Faria on 14/03/2018
+ */
+public class argumentos {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Você digitou: " + args[0]);
+    }
+    
+}
